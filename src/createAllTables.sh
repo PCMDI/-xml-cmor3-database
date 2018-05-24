f10b13bf06b5546335855ce76ebee734c9e691bb
Charles Doutriaux <doutriaux1@llnl.gov>
2018-05-23 17:23:31 -0700

    added half level atm axis entries

--- src/createAllTables.sh
+++ src/createAllTables.sh
@@ -5,7 +5,7 @@ CMIP6DB=${CMIP6DB:-CMIP6.sql3}
 for realm in $(${SQLITE3} "${CMIP6DB}" \
                 'select distinct mipTable from CMORvar'); do
     filename="CMIP6_${realm}.json"
-    #echo "Creating: ${filename}"
+    echo "Creating: ${filename}"
     echo "python CMORCreateTable.py -r ${realm} -j  > /tmp/${filename}"
     echo "python CMORCreateTable.py -r ${realm} -j  > /tmp/${filename}" |sh
 done
